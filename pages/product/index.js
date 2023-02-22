c49fe1dfdf72400c54e1b0e2c558a52e861182ce
Pithovirus <59834050+Pithovirus@users.noreply.github.com>
2023-02-22 16:48:59 +0800

    Geh lang

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,12 +1,30 @@
 import React from "react";
+import Link from "next/link";
 
-function ProductList() {
+function ProductList({ productId = 100 }) {
   return (
-    <div>
-      <h1> Product 1</h1>
-      <h1> Product 2</h1>
-      <h1> Product 3</h1>
-    </div>
+    <>
+      <Link href="/">Home</Link>
+      <h2>
+        {" "}
+        <Link href="/product/1">Product 1</Link>
+      </h2>
+      <h2>
+        {" "}
+        <Link href="/product/2">Product 2</Link>
+      </h2>
+      <h2>
+        {" "}
+        <Link href="/product/3" replace>
+          Product 3
+        </Link>
+      </h2>
+
+      <h2>
+        {" "}
+        <Link href={`/product/${productId}`}>Product {productId}</Link>
+      </h2>
+    </>
   );
 }
 
